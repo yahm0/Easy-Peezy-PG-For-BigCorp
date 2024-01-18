dae0b5424c1550f1d80feb6945e3b3fd6089658d
Piece of Cake Software <admin@pieceofcakesoftware.net>
2024-01-18 11:46:53 -0500

    =Added logic to the script.js file that parses information

--- Develop/script.js
+++ Develop/script.js
@@ -7,6 +7,15 @@ const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const numericChars = '0123456789';
 const specialChars = '!@#$%^&*()-_=+';
 
+// Variables to store user choices
+let allChars = '';
+let passwordLength;
+
+// Prompt user for password length
+do {
+  passwordLength = parseInt(prompt('Enter password length (between 8 and 128):'));
+} while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128);
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
