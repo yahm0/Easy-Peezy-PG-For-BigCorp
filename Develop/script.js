e3c49d47ce159adce3e4a9033e10f9372b0a5cb5
Piece of Cake Software <admin@pieceofcakesoftware.net>
2024-01-18 11:43:38 -0500

    Added constants to the password generator

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Define character sets for password generator
+const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const numericChars = '0123456789';
+const specialChars = '!@#$%^&*()-_=+';
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
